629f51152d05c2f2530d3dc9da67f21f4e19955d
Parth <shindeparth68412@gmail.com>
2026-02-05 22:20:21 +0530

    day 37

---
+++ Arrays/roman_to_integer.cpp
@@ -0,0 +1,39 @@
+/*
+LeetCode 13. Roman to Integer
+https://leetcode.com/problems/roman-to-integer/
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> val = {
+            {'I',1}, {'V',5}, {'X',10},
+            {'L',50}, {'C',100},
+            {'D',500}, {'M',1000}
+        };
+
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && val[s[i]] < val[s[i + 1]])
+                ans -= val[s[i]];
+            else
+                ans += val[s[i]];
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "MCMXCIV";
+    cout << sol.romanToInt(s) << endl;
+    return 0;
+}
